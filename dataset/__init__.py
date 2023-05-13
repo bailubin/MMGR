4a559d56b8fc5d692719bb36b7bde5a965179ad8
bailubin <1134673495@qq.com>
2023-05-13 10:32:40 +0800

    Add files via upload

---
+++ dataset/__init__.py
@@ -0,0 +1,1 @@
+from .poiimgaugdataset import PoiImageAugDataset
---
+++ dataset/poiimgaugdataset.py
@@ -0,0 +1,67 @@
+import os
+from PIL import Image
+from torch_geometric.data import Dataset, Data
+import torch
+from torchvision import transforms
+import random
+
+
+class PoiImageAugDataset(Dataset):
+    def __init__(self, path, edge_w=False, poi_f='poi'):
+        super(PoiImageAugDataset, self).__init__()
+        print(poi_f)
+        self.img_path = os.path.join(path, 'img')
+        self.poi_path = os.path.join(path, 'wuhanemb', poi_f)
+        self.edge_path = os.path.join(path, 'wuhanemb', 'edge')
+        self.edge_w = edge_w
+
+        if self.edge_w:
+            self.edge_w_path = os.path.join(path, 'wuhanemb', 'edgeweight')
+
+        filelist = os.path.join(path, 'all-wuhan.txt')
+
+        self.train_files = []
+        with open(filelist, 'r') as lines:
+            for line in lines:
+                self.train_files.append(line.rstrip('\n'))
+
+    def __getitem__(self, index):
+        img_name = os.path.join(self.img_path, self.train_files[index][:-3] + 'png')
+        poi_name = os.path.join(self.poi_path, self.train_files[index][:-4] + 'tensor')
+        edge_name = os.path.join(self.edge_path, self.train_files[index][:-3] + 'tensor')
+        img = Image.open(img_name)
+
+        img1 = self.random_aug(img)
+        img2 = self.random_aug(img)
+
+        # POI embedding
+        second_class_emb_list = torch.load(poi_name, map_location='cpu').float()
+        ws = torch.load(edge_name, map_location='cpu').long()
+
+        if self.edge_w:
+            edge_w_name = os.path.join(self.edge_w_path, self.train_files[index][:-3] + 'tensor')
+            edge_weight = torch.load(edge_w_name, map_location='cpu').float()
+            return Data(x=second_class_emb_list, edge_index=ws, edge_attr=edge_weight), img1, img2
+        else:
+            return Data(x=second_class_emb_list, edge_index=ws), img1, img2
+
+    def random_aug(self, img):
+        RandomResizeCrop = transforms.RandomResizedCrop(224, scale=(0.2, 1.))
+        ColorJitter = transforms.ColorJitter(0.4, 0.4, 0.4, 0.1)
+        RandomGrayscale = transforms.RandomGrayscale(p=0.2)
+        RandomHorizontalFlip = transforms.RandomHorizontalFlip()
+        toTensor = transforms.ToTensor()
+        normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                         std=[0.229, 0.224, 0.225])
+
+        img = RandomResizeCrop(img)
+        if random.random() > 0.2:
+            img = ColorJitter(img)
+        img = RandomGrayscale(img)
+        img = RandomHorizontalFlip(img)
+        img = toTensor(img)
+        img = normalize(img)
+        return img
+
+    def __len__(self):
+        return len(self.train_files)
